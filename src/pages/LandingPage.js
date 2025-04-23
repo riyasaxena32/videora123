f3d4056c68f5f9c4d94862132e2ba619ab61adc0
riyasaxena32 <122926421+riyasaxena32@users.noreply.github.com>
2025-04-23 13:15:32 +0530

    Update LandingPage.js

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -48,7 +48,7 @@ const LandingPage = () => {
         
         <div>
           <Link 
-            to="/create"
+            to="/home"
             style={gradientButtonStyle}
             className="flex items-center gap-2 text-white px-4 py-2 text-sm transition-colors font-medium"
           >
