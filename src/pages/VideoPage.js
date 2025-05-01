68dd22fed489be9790c918e85f02080320911481
riyasaxena32 <122926421+riyasaxena32@users.noreply.github.com>
2025-05-01 21:36:13 +0530

    Update VideoPage.js

--- src/pages/VideoPage.js
+++ src/pages/VideoPage.js
@@ -1139,11 +1139,9 @@ function VideoPage() {
                         >
                           {isMuted ? (
                             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="w-5 h-5">
-                              <line x1="1" y1="1" x2="23" y2="23"></line>
-                              <path d="M9 9v3a3 3 0 0 0 5.12 2.12M15 9.34V4a3 3 0 0 0-5.94-.6"></path>
-                              <path d="M17 16.95A7 7 0 0 1 5 12v-2m14 0v2a7 7 0 0 1-.11 1.23"></path>
-                              <line x1="12" y1="19" x2="12" y2="23"></line>
-                              <line x1="8" y1="23" x2="16" y2="23"></line>
+                              <polygon points="11 5 6 9 2 9 2 15 6 15 11 19 11 5"></polygon>
+                              <line x1="22" y1="9" x2="16" y2="15"></line>
+                              <line x1="16" y1="9" x2="22" y2="15"></line>
                             </svg>
                           ) : (
                             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="w-5 h-5">
@@ -1153,7 +1151,7 @@ function VideoPage() {
                           )}
                         </button>
                         
-                        {/* Volume slider - horizontal next to the speaker icon */}
+                        {/* Inline volume slider */}
                         <input 
                           type="range" 
                           min="0" 
