bedd98a437af3b725031ecef4bd17c6caa747ba4
riyasaxena32 <122926421+riyasaxena32@users.noreply.github.com>
2025-05-01 20:19:54 +0530

    kk

--- src/pages/SavedVideos.js
+++ src/pages/SavedVideos.js
@@ -378,7 +378,7 @@ function SavedVideos() {
 
               {/* Video Grid */}
               <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4 md:gap-6">
-                {videos.slice(1).map((video, index) => (
+                {videos.map((video, index) => (
                   <div 
                     key={video._id} 
                     className="cursor-pointer relative"
